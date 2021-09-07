85491347f0b44ef6a6281b65992603c1ca661069
“AlexeyRozhnov” <rozhnov.alexey@gmail.com>
2021-09-07 18:26:29 +0300

    commented and fixed new user issue

--- database/mongo.py
+++ database/mongo.py
@@ -12,13 +12,13 @@ def count():
     return res
 
 
-def find_posts(user):  # Finds posts created after latest user look-up
-    user = list(user_collection.find({"user": user}))
+def find_posts(user_id):  # Finds posts created after latest user look-up
+    user = list(user_collection.find({"user": user_id}))
     if len(user) == 1:
         user_time_stamp = user[0]["timestamp"]
     else:
-        user_collection.insert({"user": user, "timestamp": datetime.datetime.min})
-        user_time_stamp = user[0]["timestamp"]
+        user_collection.insert({"user": user_id, "timestamp": datetime.datetime.min})
+        user_time_stamp = list(user_collection.find({"user": user_id}))[0]["timestamp"]
 
     res = list(collection.find({
         "timestamp": {
